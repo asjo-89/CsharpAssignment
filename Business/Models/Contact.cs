20df2b74af9a80591e66b8bd0a8bd9d96799f938
Åsa Johansson <asa.j.1989@gmail.com>
2025-01-07 14:13:31 +0100

    Design for Add and Update contact forms and buttons. Also som icons in the contact list.

--- Business/Models/Contact.cs
+++ Business/Models/Contact.cs
@@ -8,6 +8,6 @@ public class Contact
     public string Email { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
     public string StreetAddress { get; set; } = null!;
-    public int PostalCode { get; set; }
+    public int? PostalCode { get; set; }
     public string City { get; set; } = null!;
 }
--- Business/Models/ContactForm.cs
+++ Business/Models/ContactForm.cs
@@ -7,6 +7,6 @@ public class ContactForm
     public string Email { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
     public string StreetAddress { get; set; } = null!;
-    public int PostalCode { get; set; }
+    public int? PostalCode { get; set; }
     public string City { get; set; } = null!;
 }
